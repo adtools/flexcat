7b112890ecaf0908c7d556b28aa27387b70e5bb3
Jens Maus <mail@jens-maus.de>
2011-11-26 15:25:18 +0000

    * misc: bumped revision for final 2.9 release.

--- src/version.h
+++ src/version.h
@@ -29,8 +29,8 @@
 
 // for setting all necessary version information
 #define EXE_VERSION    2
-#define EXE_REVISION   8
-#define EXE_DATE       "03.02.2011"
+#define EXE_REVISION   9
+#define EXE_DATE       "26.11.2011"
 #define EXE_COPYRIGHT  "Copyright (C) 2005-2011 FlexCat Open Source Team"
 
 // set the EXE_REV_STRING
