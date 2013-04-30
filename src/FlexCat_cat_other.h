36b0349478550826098495ba6176878aff187c25
Thore Böckelmann <tboeckel@gmx.de>
2013-04-30 20:16:10 +0000

    * catalogs/greek.ct: use codeset 10 for the greek translation.

--- src/FlexCat_cat_other.h
+++ src/FlexCat_cat_other.h
@@ -1,7 +1,7 @@
 
 /****************************************************************
 
-   This file was created automatically by `FlexCat 2.10'
+   This file was created automatically by `FlexCat 2.11'
    from "catalogs/FlexCat.cd".
 
    Do NOT edit by hand!
